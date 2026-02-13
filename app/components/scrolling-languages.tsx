4054a8666b0a4330d84cd722b57f3dda32df60e0
Gabriel Santos <145061665+Gabe575@users.noreply.github.com>
2026-02-12 22:40:01 -0500

    added eslint, prettier, husky, resolved lint errors and refactored code with prettier
    * added eslint, prettier, and husky to project
    
    * resolving all warnings and refactoring code with prettier

--- app/components/scrolling-languages.tsx
+++ app/components/scrolling-languages.tsx
@@ -1,63 +1,72 @@
-import Image from "next/image";
-import Marquee from "@components/marquee";
+import Image from 'next/image';
+import Marquee from '@components/marquee';
 
 const languages = [
-  { name: "C#", icon: <Image src="/csharp.svg" alt="C#" width={48} height={48}/> },
-  { name: "C++", icon: <Image src="/cplusplus.svg" alt="C++" width={48} height={48}/> },
-  { name: "Git", icon: <Image src="/git.svg" alt="Git" width={48} height={48}/> },
-  { name: "Java", icon: <Image src="/java.svg" alt="Java" width={48} height={48}/> },
-  { name: "JavaScript", icon: <Image src="/javascript.svg" alt="JavaScript" width={48} height={48}/> },
-  { name: "Kotlin", icon: <Image src="/kotlin.svg" alt="Kotlin" width={48} height={48}/> },
-  { name: "Python", icon: <Image src="/python.svg" alt="Python" width={48} height={48}/> },
-  { name: "TypeScript", icon: <Image src="/typescript.svg" alt="TypeScript" width={48} height={48}/> },
-  { name: "Visual Basic", icon: <Image src="/visualbasic.svg" alt="Visual Basic" width={48} height={48}/> },
+  { name: 'C#', icon: <Image src="/csharp.svg" alt="C#" width={48} height={48} /> },
+  { name: 'C++', icon: <Image src="/cplusplus.svg" alt="C++" width={48} height={48} /> },
+  { name: 'Git', icon: <Image src="/git.svg" alt="Git" width={48} height={48} /> },
+  { name: 'Java', icon: <Image src="/java.svg" alt="Java" width={48} height={48} /> },
+  {
+    name: 'JavaScript',
+    icon: <Image src="/javascript.svg" alt="JavaScript" width={48} height={48} />,
+  },
+  { name: 'Kotlin', icon: <Image src="/kotlin.svg" alt="Kotlin" width={48} height={48} /> },
+  { name: 'Python', icon: <Image src="/python.svg" alt="Python" width={48} height={48} /> },
+  {
+    name: 'TypeScript',
+    icon: <Image src="/typescript.svg" alt="TypeScript" width={48} height={48} />,
+  },
+  {
+    name: 'Visual Basic',
+    icon: <Image src="/visualbasic.svg" alt="Visual Basic" width={48} height={48} />,
+  },
 ];
 const frameworks = [
-  { name: ".NET", icon: <Image src="/dotnet.svg" alt=".NET" width={48} height={48}/> },
-  { name: "Angular", icon: <Image src="/angular.svg" alt="Angular" width={48} height={48}/> },
-  { name: "Django", icon: <Image src="/django.svg" alt="Django" width={48} height={48}/> },
-  { name: "MongoDB", icon: <Image src="/mongodb.svg" alt="MongoDB" width={48} height={48}/> },
-  { name: "NextJS", icon: <Image src="/nextjs.svg" alt="NextJS" width={48} height={48}/> },
-  { name: "NodeJS", icon: <Image src="/nodejs.svg" alt="NodeJS" width={48} height={48}/> },
-  { name: "Postgres", icon: <Image src="/postgres.svg" alt="Postgres" width={48} height={48}/> },
-  { name: "React", icon: <Image src="/react.svg" alt="React" width={48} height={48}/> },
-  { name: "Unity", icon: <Image src="/unity.svg" alt="Unity" width={48} height={48}/> },
+  { name: '.NET', icon: <Image src="/dotnet.svg" alt=".NET" width={48} height={48} /> },
+  { name: 'Angular', icon: <Image src="/angular.svg" alt="Angular" width={48} height={48} /> },
+  { name: 'Django', icon: <Image src="/django.svg" alt="Django" width={48} height={48} /> },
+  { name: 'MongoDB', icon: <Image src="/mongodb.svg" alt="MongoDB" width={48} height={48} /> },
+  { name: 'NextJS', icon: <Image src="/nextjs.svg" alt="NextJS" width={48} height={48} /> },
+  { name: 'NodeJS', icon: <Image src="/nodejs.svg" alt="NodeJS" width={48} height={48} /> },
+  { name: 'Postgres', icon: <Image src="/postgres.svg" alt="Postgres" width={48} height={48} /> },
+  { name: 'React', icon: <Image src="/react.svg" alt="React" width={48} height={48} /> },
+  { name: 'Unity', icon: <Image src="/unity.svg" alt="Unity" width={48} height={48} /> },
 ];
 
 const ScrollingLanguages = () => {
   return (
     <>
       <div className="relative flex flex-col justify-between items-center h-48 w-full h-68 min-h-68 md:hidden">
-          <Marquee direction="left" gradient gradientWidth={100}> 
-            {languages.map((lang, idx) => (
-              <div key={idx} className="languages-div mr-2">
-                {lang.icon} {lang.name}
-              </div>
-            ))}
-          </Marquee>
-          <Marquee direction="right" gradient gradientWidth={100}>
-            {frameworks.map((lang, idx) => (
-              <div key={idx} className="languages-div mr-2">
-                {lang.icon} {lang.name}
-              </div>
-            ))}
-          </Marquee>
+        <Marquee direction="left" gradient gradientWidth={100}>
+          {languages.map((lang, idx) => (
+            <div key={idx} className="languages-div mr-2">
+              {lang.icon} {lang.name}
+            </div>
+          ))}
+        </Marquee>
+        <Marquee direction="right" gradient gradientWidth={100}>
+          {frameworks.map((lang, idx) => (
+            <div key={idx} className="languages-div mr-2">
+              {lang.icon} {lang.name}
+            </div>
+          ))}
+        </Marquee>
       </div>
       <div className="relative flex flex-row justify-between items-start w-68 min-w-68 -mt-24 max-md:hidden h-207 lg:h-256 lg:w-96 lg:min-w-96 xl:w-140 xl:min-w-140">
-          <Marquee direction="up"  gradient> 
-            {languages.map((lang, idx) => (
-              <div key={idx} className="languages-div mb-2 xl:mb-4">
-                {lang.icon} {lang.name}
-              </div>
-            ))}
-          </Marquee>
-          <Marquee direction="down"  gradient>
-            {frameworks.map((lang, idx) => (
-              <div key={idx} className="languages-div mb-2 xl:mb-4">
-                {lang.icon} {lang.name}
-              </div>
-            ))}
-          </Marquee>
+        <Marquee direction="up" gradient>
+          {languages.map((lang, idx) => (
+            <div key={idx} className="languages-div mb-2 xl:mb-4">
+              {lang.icon} {lang.name}
+            </div>
+          ))}
+        </Marquee>
+        <Marquee direction="down" gradient>
+          {frameworks.map((lang, idx) => (
+            <div key={idx} className="languages-div mb-2 xl:mb-4">
+              {lang.icon} {lang.name}
+            </div>
+          ))}
+        </Marquee>
       </div>
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,83 @@
-import { FiGithub, FiLinkedin } from "react-icons/fi";
-import Image from "next/image";
-import ScrollingLanguages from "@components/scrolling-languages";
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
+import Image from 'next/image';
+import ScrollingLanguages from '@components/scrolling-languages';
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center">
       <main className="flex min-h-screen w-full flex-col items-center sm:items-start">
-        <section id="home" className="min-h-screen w-full pt-24 px-6 flex flex-col items-center text-center sm:items-start sm:text-left sm:px-16 md:pt-32">
+        <section
+          id="home"
+          className="min-h-screen w-full pt-24 px-6 flex flex-col items-center text-center sm:items-start sm:text-left sm:px-16 md:pt-32"
+        >
           <div className="max-w-7xl mx-auto w-full h-full items-center flex flex-col sm:items-start sm:text-left">
             <h1 className="max-w-xs text-4xl font-semibold leading-10 tracking-tight whitespace-nowrap sm:text-7xl">
               Gabriel Santos
             </h1>
-            <p className="max-w-md text-lg leading-8 text-zinc-500">
-              Software Engineer
-            </p>
+            <p className="max-w-md text-lg leading-8 text-zinc-500">Software Engineer</p>
           </div>
         </section>
 
-        <section id="about" className="bg-darkish w-full pt-16 flex flex-col items-center text-center bg-black md:items-start md:text-left md:px-16 md:pt-24">
+        <section
+          id="about"
+          className="bg-darkish w-full pt-16 flex flex-col items-center text-center bg-black md:items-start md:text-left md:px-16 md:pt-24"
+        >
           <div className="max-w-7xl mx-auto w-full h-full">
             <div className="flex flex-col items-center justify-between md:flex-row md:items-start gap-8 max-md:pb-16 ">
               <div className="items-center text-center max-sm:px-6">
                 <h2 className="section-title md:text-left">About</h2>
                 <div className="inline-block p-1 mb-4 rounded-full bg-gradient-to-r from-pink-400 via-indigo-400 to-emerald-400 animate-border">
                   <div className="rounded-full overflow-hidden max-w-32 sm:max-w-48 lg:max-w-96">
-                    <Image src="/headshot.jpg" alt="Gabriel Santos" width={480} height={480}/>
+                    <Image src="/headshot.jpg" alt="Gabriel Santos" width={480} height={480} />
                   </div>
                 </div>
-                <p className="lg:text-xl">Hey! I'm Gabe, a software engineer and full stack developer based&nbsp;in</p>
-                <p className="whitespace-nowrap text-lg lg:text-2xl"><b>Ontario, Canada</b></p>
+                <p className="lg:text-xl">
+                  Hey! I&apos;m Gabe, a software engineer and full stack developer based&nbsp;in
+                </p>
+                <p className="whitespace-nowrap text-lg lg:text-2xl">
+                  <b>Ontario, Canada</b>
+                </p>
                 <div className="md:text-start py-4 max-sm:px-6 max-md:px-16 md:py-8">
                   <p className="text-sm sm:text-base lg:text-lg text-zinc-700 dark:text-zinc-400">
-                    I've gained experience in a variety of programming languages and frameworks throughout my time working in internships, personal projects, and studying{" "}
+                    I&apos;ve gained experience in a variety of programming languages and frameworks
+                    throughout my time working in internships, personal projects, and studying{' '}
                     <b>Software Engineering</b> at <b>Western University</b>.
                   </p>
                 </div>
               </div>
-              <ScrollingLanguages/>
+              <ScrollingLanguages />
             </div>
           </div>
         </section>
 
-        <section id="projects" className="min-h-screen w-full pt-16 px-6 flex flex-col items-center text-center sm:items-start sm:text-left sm:px-16 md:pt-24">
+        <section
+          id="projects"
+          className="min-h-screen w-full pt-16 px-6 flex flex-col items-center text-center sm:items-start sm:text-left sm:px-16 md:pt-24"
+        >
           <div className="max-w-7xl mx-auto w-full h-full">
             <h2 className="section-title">Projects</h2>
           </div>
         </section>
 
-        <section id="contact" className="bg-darkish min-h-screen w-full pt-16 px-6 flex flex-col items-center text-center sm:items-start sm:text-left sm:px-16 md:pt-24">
+        <section
+          id="contact"
+          className="bg-darkish min-h-screen w-full pt-16 px-6 flex flex-col items-center text-center sm:items-start sm:text-left sm:px-16 md:pt-24"
+        >
           <div className="max-w-7xl mx-auto w-full h-full">
             <h2 className="section-title">Contact</h2>
           </div>
         </section>
-        
-        
       </main>
       <footer className="py-12 px-6 text-sm text-zinc-500 text-center w-full bg-zinc-200 dark:bg-zinc-900 shadow-md transition-all duration-300 sm:px-16">
         <div className="max-w-7xl mx-auto flex flex-row items-center justify-between">
           <p>© {new Date().getFullYear()} Gabriel Santos. Built with Next.js.</p>
           <div className="flex justify-center gap-4">
-            <a href="https://github.com/Gabe575" aria-label="GitHub"><FiGithub/></a>
-            <a href="https://linkedin.com/in/gabriel2004santos" aria-label="LinkedIn"><FiLinkedin/></a>
+            <a href="https://github.com/Gabe575" aria-label="GitHub">
+              <FiGithub />
+            </a>
+            <a href="https://linkedin.com/in/gabriel2004santos" aria-label="LinkedIn">
+              <FiLinkedin />
+            </a>
           </div>
         </div>
       </footer>
